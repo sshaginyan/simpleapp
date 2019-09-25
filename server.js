e37807027af7fda4ecb33544c0e950bdcfcb83c8
Steven Shaginyan <sshaginyan@Gmail.com>
2019-09-25 11:28:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-require('http').createServer((req, res) => { res.end('Hey! @123 okay! hello ABC'); }).listen(process.env.PORT || 8080);
+require('http').createServer((req, res) => { res.end('Hello World'); }).listen(process.env.PORT || 8080);
